b9b3732cd1d5aa132392b93f845a2522c6bb2409
Rebeca Queiroz <contact@rebecaqueiroz.com>
2026-02-04 12:47:20 -0300

    updating theme with the latest framework

--- functions.php
+++ functions.php
@@ -39,8 +39,8 @@ function gx_login_logo() {?>
       }
     </style>
 <?php }
-add_action( 'login_enqueue_scripts', 'gx_login_logo' );
 
+add_action( 'login_enqueue_scripts', 'gx_login_logo' );
 function gxcore_enqueue_styles() {
     $theme_version = wp_get_theme()->get('Version');
 
@@ -52,16 +52,43 @@ function gxcore_enqueue_styles() {
         $theme_version
     );
 
-    // Enqueue components.css
+    // Enqueue inherited components.css
     wp_enqueue_style(
         'gxcore-components',
         get_stylesheet_directory_uri() . '/css/components.css',
-        ['gxcore-base'], // Optional dependency
+        ['gxcore-base'],
+        $theme_version
+    );
+
+    // Enqueue new components.css
+    wp_enqueue_style(
+        'aquaerial-components',
+        get_stylesheet_directory_uri() . '/components.css',
+        [],
         $theme_version
     );
 }
 add_action('wp_enqueue_scripts', 'gxcore_enqueue_styles');
 
+
+function google_tag_manager() { ?>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K63Q8JRC');</script>
+<!-- End Google Tag Manager -->
+<?php }
+
+add_action( 'wp_head', 'google_tag_manager' );
+function google_tag_manager_body() { ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K63Q8JRC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<?php }
+add_action('astra_body_top', 'google_tag_manager_body');
 
 /**
  * Enqueue styles
